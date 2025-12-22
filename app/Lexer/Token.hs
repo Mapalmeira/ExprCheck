ea3ef0f140a83523ff75d97085da42addf6733fa
Mapalmeira <matheus.palmeira.leite@gmail.com>
2025-12-22 11:11:17 -0300

    feat: defining tokens

--- app/Lexer/Token.hs
+++ app/Lexer/Token.hs
@@ -2,4 +2,25 @@ module Lexer.Token where
 
 data Token
   = TokEOF
+  | TokLParen
+  | TokRParen
+  | TokStar
+  | TokSlash
+  | TokCaret
+  | TokPlus
+  | TokMinus
+  | TokInt Int
+  | TokReal Double
   deriving (Eq)
+
+instance Show Token where
+  show TokEOF      = "EOF"
+  show TokLParen   = "("
+  show TokRParen   = ")"
+  show TokStar     = "*"
+  show TokSlash    = "/"
+  show TokCaret    = "^"
+  show TokPlus     = "+"
+  show TokMinus    = "-"
+  show (TokInt n)  = "INT(" ++ show n ++ ")"
+  show (TokReal n)  = "REAL(" ++ show n ++ ")"

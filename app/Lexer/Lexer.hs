2eeaf8cd3c965a9e97ced10df41292d49cbf5bf7
Erik <eriikalves55@gmail.com>
2025-12-26 13:31:19 -0300

    feat: Lexer para validação lexicográfica das expressões matemáticas.

--- app/Lexer/Lexer.hs
+++ app/Lexer/Lexer.hs
@@ -1,6 +1,43 @@
 module Lexer.Lexer where
 
+import Data.Char (isDigit, isSpace)
 import Lexer.Token
 
-lexAll :: String -> [Token]
-lexAll _ = [TokEOF]
+lexer :: String -> Either String [Token]
+lexer [] = Right [TokEOF]
+lexer (c:cs)
+    | isSpace c = lexer cs
+    | isDigit c = lexerNum (c:cs)
+    | otherwise = case c of
+        '+' -> addToken TokPlus
+        '-' -> addToken TokMinus
+        '*' -> addToken TokStar
+        '/' -> addToken TokSlash
+        '^' -> addToken TokCaret
+        '(' -> addToken TokLParen
+        ')' -> addToken TokRParen
+        _   -> Left $ "Erro Léxico: Caractere inválido encontrado: '" ++ [c] ++ "'"
+  where
+    addToken tok =
+        case lexer cs of
+            Left err   -> Left err
+            Right toks -> Right (tok : toks)
+
+-- Processa números inteiros e reais
+lexerNum :: String -> Either String [Token]
+lexerNum cs =
+    let (numPart, rest) = span isDigit cs
+    in case rest of
+        ('.':restAfterDot) ->
+            let (fracPart, restAfterFrac) = span isDigit restAfterDot
+            in if null fracPart
+               then Left "Erro Léxico: Número real mal formado (terminado em ponto)"
+               else case restAfterFrac of
+                   ('.':_) -> Left "Erro Léxico: Número real mal formado (múltiplos pontos)"
+                   _ -> case lexer restAfterFrac of
+                       Left err   -> Left err
+                       Right toks -> Right (TokReal (read (numPart ++ "." ++ fracPart)) : toks)
+
+        _ -> case lexer rest of
+            Left err   -> Left err
+            Right toks -> Right (TokInt (read numPart) : toks)
--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,32 @@ module Main where
 
 import Lexer.Lexer
 
+testCases :: [String]
+testCases =
+  [ "3.45 + 10"
+  , "1+2"
+  , "(1 + 2) * 3"
+  , "10 / (5 - 3)"
+  , "2^3 + 4"
+  , "123"
+  , "0.5 + 0.25"
+  , "((1))"
+  -- inválidos
+  , "1 + 2.3.4"
+  , "3."
+  , ".5"
+  , "2 + a"
+  , "5 + @"
+  , "1..2"
+  , "(1 + 2"
+  ]
+
+runTest :: String -> IO ()
+runTest input = do
+  putStrLn $ "\nEntrada: " ++ show input
+  case lexer input of
+    Left err     -> putStrLn $ "Erro: " ++ err
+    Right tokens -> putStrLn $ "Tokens: " ++ show tokens
+
 main :: IO ()
-main = print (lexAll "5+4")
+main = mapM_ runTest testCases

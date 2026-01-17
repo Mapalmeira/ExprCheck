69f7311e6378fa444564959e9546c7dfe651ee31
ErikAlvesAlmeida <eriikalves55@gmail.com>
2026-01-17 16:47:13 -0300

    feat: propagate typed parser errors and update parser tests

--- test/ParserTests.hs
+++ test/ParserTests.hs
@@ -6,34 +6,46 @@ import Parser.Parser
 import Parser.AST
 
 -- Dense tests
-parseString :: String -> Either String Exp
+parseString :: String -> Either String Ast
 parseString input =
   case lexer input of
     Left err -> Left (show err)
-    Right tokens -> parse tokens
+    Right tokens ->
+      case parse tokens of
+        Left err  -> Left (show err)
+        Right ast -> Right ast
 
 testPrecedence :: Test
 testPrecedence =
   let input = "1 + 2 * 3"
       expected = Right (Binary Plus (IntVal 1) (Binary Star (IntVal 2) (IntVal 3)))
-  in assertEquals ("Parser aceita entrada válida: " ++ input ++ " com saída verificada") expected (parseString input)
+  in assertEquals
+       ("Parser aceita entrada válida: " ++ input ++ " com saída verificada")
+       expected
+       (parseString input)
 
 testParen :: Test
 testParen =
   let input = "(1 + 2) * 3"
       expected = Right (Binary Star (Binary Plus (IntVal 1) (IntVal 2)) (IntVal 3))
-  in assertEquals ("Parser aceita entrada válida: " ++ input ++ " com saída verificada") expected (parseString input)
+  in assertEquals
+       ("Parser aceita entrada válida: " ++ input ++ " com saída verificada")
+       expected
+       (parseString input)
 
 testPow :: Test
 testPow =
   let input = "4 * 2 ^ 3"
       expected = Right (Binary Star (IntVal 4) (Binary Caret (IntVal 2) (IntVal 3)))
-  in assertEquals ("Parser aceita entrada válida: " ++ input ++ " com saída verificada") expected (parseString input)
+  in assertEquals
+       ("Parser aceita entrada válida: " ++ input ++ " com saída verificada")
+       expected
+       (parseString input)
 
 -- Quick tests
-otherValidInputs = 
+otherValidInputs =
   [ "5"
-  ,  "+5"
+  , "+5"
   , "5+3"
   , "5.3 + 2"
   , "5.3^2"
@@ -48,7 +60,7 @@ otherValidInputs =
   , "5 ++ 5"
   ]
 
-otherInvalidInputs = 
+otherInvalidInputs =
   [ "()"
   , "5+-+-+-+-+-5"
   , "( 5 * 2"
@@ -66,7 +78,6 @@ parserFails input =
     ("Parser rejeita entrada inválida: " ++ show input)
     (parseString input)
 
-
 -- Test suite
 data ParserTests = ParserTests
 
@@ -76,7 +87,5 @@ instance TestSuite ParserTests where
     , testParen
     , testPow
     ]
-    ++
-    map parserSucceeds otherValidInputs
-    ++
-    map parserFails otherInvalidInputs
+    ++ map parserSucceeds otherValidInputs
+    ++ map parserFails otherInvalidInputs
